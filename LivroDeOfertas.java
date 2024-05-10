2cc4a1e09cef362b8dd89f830fd18e88ead447ae
valdecirsarat <77338085+valdecirsarat@users.noreply.github.com>
2024-05-10 15:53:09 -0300

    Add files via upload

---
+++ LivroDeOfertas.java
@@ -0,0 +1,141 @@
+package desafio;
+
+import java.util.InputMismatchException;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class LivroDeOfertas {
+	
+	Integer posicao;
+	Double valor;
+	Double quantidade;
+	
+	LivroDeOfertas[] livros;
+	
+	public LivroDeOfertas() {}
+	
+	public LivroDeOfertas(Integer posicao, Double valor, Double quantidade) {
+		this.posicao = posicao;
+		this.valor = valor;
+		this.quantidade = quantidade;
+	}
+	
+	
+	public Integer getPosicao() {
+		return posicao;
+	}
+
+	public void setPosicao(Integer posicao) {
+		this.posicao = posicao;
+	}
+
+	public Double getValor() {
+		return valor;
+	}
+
+	public void setValor(Double valor) {
+		this.valor = valor;
+	}
+
+	public Double getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(Double quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public LivroDeOfertas[] getLivros() {
+		return livros;
+	}
+
+	public void setLivros(LivroDeOfertas[] livros) {
+		this.livros = livros;
+	}
+	
+
+	@Override
+	public String toString() {
+		return "posicao=" + posicao + ", valor=" + valor + ", quantidade=" + quantidade + "";
+	}
+	
+	
+	public void create(int posicao,LivroDeOfertas livro) {
+		
+	        this.livros[posicao -1]= livro;
+	
+	}
+	
+	public void update(int posicao, double valor , double quantidade) {
+		LivroDeOfertas livro = livros[posicao-1];
+		livro.setQuantidade(quantidade);
+		livro.setValor(valor);
+	}
+
+	public void delete(int posicao) {
+		this.livros[posicao - 1]= null;
+		
+	}
+//--------------------------------------------------------
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		sc.useLocale(Locale.US);
+		LivroDeOfertas livroDeOfertas = new LivroDeOfertas();
+
+		try {
+			System.out.println("Notificação: ");		
+			int notificacao = sc.nextInt();
+			livroDeOfertas.livros = new LivroDeOfertas[notificacao];
+			
+			for(int i = 1; i <= notificacao ; i++) {				
+					String entrada = sc.next();
+					String[] auxliar = entrada.split(",");
+					int posicao = Integer.parseInt(auxliar[0]);
+					int acao =Integer.parseInt(auxliar[1]);
+					double valor = Double.parseDouble(auxliar[2]);
+					double quantidade = Double.parseDouble(auxliar[3]);
+					
+					switch (acao) {
+					//inserido um novo
+						case 0: {
+							LivroDeOfertas novo = new LivroDeOfertas(posicao, valor, quantidade);
+							livroDeOfertas.create(posicao,novo);
+							break;
+							}
+					// update
+						case 1:{
+							System.out.println("update");
+							livroDeOfertas.update(posicao,valor, quantidade);
+							break;
+							}
+					//delete
+						case 2:{
+							System.out.println("delete");
+							livroDeOfertas.delete(posicao);
+							break;
+						}
+					}
+									
+			}
+			sc.close();
+			
+			for (LivroDeOfertas livro : livroDeOfertas.getLivros()) {
+				if(livro != null) {
+					System.out.println(livro);
+				}
+				  
+			}
+			
+		}catch (InputMismatchException e) {
+			//verificando o erro que ocasionava na utlização do scanner
+			System.out.print(e.getMessage());
+		}
+		
+				
+		
+	}
+
+}
---
+++ MensagemSecreta.java
@@ -0,0 +1,48 @@
+package desafio;
+
+public class MensagemSecreta {
+	
+	public static void main(String[] args) {
+		String msg = "10010110 11110111 01010110 00000001 00010111 00100110 01010111 00000001 00010111 01110110 01010111 00110110 11110111 11010111 01010111 00000011";
+		String[] msgSeparada = msg.split(" ");	
+		mudaBits(msgSeparada);
+		imprimiMsg(msgSeparada);
+		
+		
+	 
+		
+	}
+	
+	public static void  mudaBits(String[] msg) {
+		
+		for(int i=0; msg.length > i; i++) {
+			char[] msgAux = msg[i].toCharArray();
+			if(msgAux[6] == '0') {
+				msgAux[6]='1';
+			}else {
+				msgAux[6]='0';
+			}
+			if(msgAux[7]=='0') {
+				msgAux[7] = '1';
+			}else {
+				msgAux[7] = '0';
+			}
+			
+			msg[i] = new String(msgAux);
+			
+			}		
+	}
+	
+	public static void imprimiMsg(String[] msg) {
+		for(int i = 0; msg.length > i; i++) {
+			String bitsIniciais = msg[i].substring(0,4);
+			String bitsFinais = msg[i].substring(4,8);		
+			//passando para inteiro
+			int binarioParaInteiro = Integer.parseInt(bitsFinais+bitsIniciais,2);
+			
+			//imprimi msgDescriptografada
+			System.out.print((char)binarioParaInteiro);			
+		}
+	}
+
+}
